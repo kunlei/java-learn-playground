d3e73d3e73e15e59f4978acb66615507eb08a993
Kunlei Lian <kunlei1987@gmail.com>
2022-01-03 08:13:52 -0600

    learn

--- src/main/java/com/playground/java/generics/Box.java
+++ src/main/java/com/playground/java/generics/Box.java
@@ -1,6 +1,6 @@
 package com.playground.java.generics;
 
-import java.util.Comparator;
+import java.util.*;
 
 public class Box implements Comparable<Box> {
   private final int length;
@@ -15,5 +15,26 @@ public class Box implements Comparable<Box> {
   @Override
   public int compareTo(Box o) {
     return Integer.compare(this.length, o.length);
+  }
+
+  public static void main(String[] args) {
+    List<Integer> ints = Arrays.asList(1, 2, 3);
+    int s = 0;
+    for (Integer n : ints) {
+      s += n;
+    }
+    assert s == 6;
+
+    s = 0;
+    for (Iterator<Integer> it = ints.iterator(); it.hasNext();) {
+      s += it.next();
+    }
+    assert s == 6;
+
+    List<String> words = new ArrayList<>();
+    words.add("Hello ");
+    words.add("world!");
+    String ss = words.get(0) + words.get(1);
+    assert ss.equals("Hello world!");
   }
 }
