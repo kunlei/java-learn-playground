aa8533e34b1dc94216be5d5bfcbc384fbda719ec
Kunlei Lian <kunlei1987@gmail.com>
2022-01-06 20:07:42 -0600

    update

--- src/main/java/com/playground/java/generics/Wildcards.java
+++ src/main/java/com/playground/java/generics/Wildcards.java
@@ -2,9 +2,14 @@ package com.playground.java.generics;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 
 public class Wildcards {
+  public static void addAll(List<Number> source, Collection<? extends Number> input) {
+    source.addAll(input);
+  }
+
   public static void main(String[] args) {
     List<Number> numbers = new ArrayList<>();
     List<Integer> integers = Arrays.asList(1, 2, 3);
@@ -13,10 +18,22 @@ public class Wildcards {
     numbers.addAll(doubles);
     numbers.forEach(System.out::print);
 
-    List<Integer> ints = new ArrayList<>();
-    ints.add(1);
-    ints.add(2);
-    List<? extends Number> nums = ints;
-    nums.add(3);
+    List<? extends Number> list;
+    list = integers;
+    list = doubles;
+
+    addAll(numbers, integers);
+    addAll(numbers, doubles);
+
+    List<Number> nums = new ArrayList<>();
+    List<? super Number> destination = nums;
+    List<? extends Number> source = nums;
+    for (int i = 0; i < 10; i++) {
+      destination.add(i);
+    }
+    double sum = 0;
+    for (Number num : source) {
+      sum += num.doubleValue();
+    }
   }
 }
