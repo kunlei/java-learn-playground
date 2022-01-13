4e296ac4583e34fedb76e62b6b0f3576bd7c64bc
Kunlei Lian <kunlei1987@gmail.com>
2022-01-12 20:27:19 -0600

    update

---
+++ src/main/java/com/playground/java/generics/constructors/Pair.java
@@ -0,0 +1,32 @@
+package com.playground.java.generics.constructors;
+
+import java.net.Inet4Address;
+import java.util.Arrays;
+import java.util.List;
+
+public class Pair<T, U> {
+  private final T first;
+  private final U second;
+
+  public Pair(T first, U second) {
+    this.first = first;
+    this.second = second;
+  }
+
+  public T getFirst() {
+    return first;
+  }
+
+  public U getSecond() {
+    return second;
+  }
+
+  public static void main(String[] args) {
+    Pair<String, Integer> pair = new Pair<>("one", 2);
+    assert pair.getFirst().equals("one") && pair.getSecond() == 2;
+
+    List<Integer> ints = Arrays.asList(1, 2, 3);
+    List<String> strings = Arrays.asList("one", "two");
+    assert ints.getClass() == strings.getClass();
+  }
+}
---
+++ src/main/java/com/playground/java/generics/enums/Season.java
@@ -0,0 +1,8 @@
+package com.playground.java.generics.enums;
+
+public class Season {
+
+  public static void main(String[] args) {
+    Enum a;
+  }
+}
