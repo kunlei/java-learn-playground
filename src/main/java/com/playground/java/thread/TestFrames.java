1e650504127e3f885eb203bbfaf42ffb049b6f59
Kunlei Lian <kunlei1987@gmail.com>
2022-03-06 15:18:01 -0600

    update

---
+++ src/main/java/com/playground/java/thread/TestFrames.java
@@ -0,0 +1,30 @@
+package com.playground.java.thread;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class TestFrames {
+  public static void main(String[] args) {
+    Thread t1 = new Thread() {
+      @Override
+      public void run() {
+        method1(20);
+      }
+    };
+    t1.setName("t1");
+    t1.start();
+
+    method1(10);
+  }
+
+  private static void method1(int x) {
+    int y = x + 1;
+    Object m = method2();
+    System.out.println(m);
+  }
+
+  private static Object method2() {
+    Object n = new Object();
+    return n;
+  }
+}
